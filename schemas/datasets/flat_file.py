faf20bd701223a92ea28b0be43c0bf9423d2cc58
Austin Osborne <austin@goaiq.com>
2020-04-14 18:03:39 -0400

    added jobs to flat file datasets

--- schemas/datasets/flat_file.py
+++ schemas/datasets/flat_file.py
@@ -1,9 +1,12 @@
 from db import ma
 from models.datasets.flat_file import FlatFileDatasetModel
+from schemas.job import JobSchema
 
 
 class FlatFileDatasetSchema(ma.ModelSchema):
     class Meta:
         model = FlatFileDatasetModel
-        fields = ('dataset_name', 'uploader', 'created_ts', 'dataset_id', 'location')
-        dump_only = ('created_ts', 'dataset_id')
+        fields = ('dataset_name', 'uploader', 'created_ts', 'dataset_id', 'location', 'jobs')
+        dump_only = ('created_ts', 'dataset_id', 'jobs')
+    
+    jobs = ma.List(ma.Nested(JobSchema))
