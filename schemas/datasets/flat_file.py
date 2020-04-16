ed0f012e28709e0ee34860eb1e426184cb15db32
Austin Osborne <austin@goaiq.com>
2020-04-15 22:06:59 -0400

    added owners to dataset schema

--- schemas/datasets/flat_file.py
+++ schemas/datasets/flat_file.py
@@ -6,7 +6,7 @@ from schemas.job import JobSchema
 class FlatFileDatasetSchema(ma.ModelSchema):
     class Meta:
         model = FlatFileDatasetModel
-        fields = ('dataset_name', 'uploader', 'created_ts', 'dataset_id', 'location', 'jobs')
-        dump_only = ('created_ts', 'dataset_id', 'jobs')
+        fields = ('dataset_name', 'uploader', 'created_ts', 'dataset_id', 'location', 'jobs', 'owners')
+        dump_only = ('created_ts', 'dataset_id', 'jobs', 'owners')
     
     jobs = ma.List(ma.Nested(JobSchema))
