5659ba08bcca2567821dad68a0ce94608e9f912c
Austin Osborne <austin@goaiq.com>
2020-04-15 20:03:55 -0400

    fixed job schema error

--- schemas/job.py
+++ schemas/job.py
@@ -5,4 +5,5 @@ from models.job import JobModel
 class JobSchema(ma.ModelSchema):
     class Meta:
         model = JobModel
+        fields = ('job_status', 'job_created_ts', 'job_id', 'dataset_id', 'job_completed_ts')
         dump_only = ('job_status', 'job_created_ts', 'job_id', 'job_completed_ts')
