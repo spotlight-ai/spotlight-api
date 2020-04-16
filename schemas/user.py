ee487a83fda4f7807a6872973a874eef277c6b6c
Austin Osborne <austin@goaiq.com>
2020-04-15 22:12:09 -0400

    fixed user schema

--- schemas/user.py
+++ schemas/user.py
@@ -5,5 +5,5 @@ from models.user import UserModel
 class UserSchema(ma.ModelSchema):
     class Meta:
         model = UserModel
+        fields = ('first_name', 'last_name', 'roles', 'datasets_owned', 'email', 'created_ts', 'last_login')
         load_only = ('password',)
-
