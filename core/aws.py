0477d08b34186ffad93af9b8fb0fbd1ce1e723d0
Parker Sarsfield <phsarsfield@gmail.com>
2020-05-08 16:53:57 -0400

    add dataset download link

--- core/aws.py
+++ core/aws.py
@@ -2,6 +2,25 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
+def generate_presigned_download_link(bucket_name, object_name, expiration=3600):
+    """
+    Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+    s3_client = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+    try:
+        response = s3_client.generate_presigned_url('get_object', Params={ 'Bucket': bucket_name, 'Key': object_name},
+                                                     ExpiresIn=expiration)
+    except ClientError as e:
+        return None
+
+    return response
+
 
 def generate_presigned_link(bucket_name, object_name, fields=None, conditions=None, expiration=3600):
     """
