15849ce0458440c9c353e399665698613eab752d
Austin Osborne <austin@goaiq.com>
2020-05-28 10:27:25 -0400

    added additional pii markers to script and removed test config from test_main

--- scripts/preload_pii_markers.sql
+++ scripts/preload_pii_markers.sql
@@ -3,4 +3,7 @@ INSERT INTO pii_marker_base VALUES
     (2, 'ein'),
     (3, 'ssn'),
     (4, 'cc_number'),
-    (5, 'usa_phone')
+    (5, 'usa_phone'),
+    (6, 'country'),
+    (7, 'city'),
+    (8, 'gender')
