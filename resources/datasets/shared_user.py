788e209174a173ef9ff47cadf12ef165dd87355f
Austin Osborne <austin@goaiq.com>
2020-05-28 19:17:41 -0400

    added the ability to share permissions with individuals

--- resources/datasets/shared_user.py
+++ resources/datasets/shared_user.py
@@ -5,6 +5,7 @@ from core.decorators import authenticate_token
 from db import db
 from models.datasets.base import DatasetModel
 from models.datasets.shared_user import SharedDatasetUserModel
+from models.pii.pii import PIIModel
 from models.user import UserModel
 from schemas.datasets.shared_user import SharedDatasetUserSchema
 
@@ -32,22 +33,55 @@ class DatasetSharedUserCollection(Resource):
         if user not in dataset.owners:
             abort(401, 'User does not have permission to view users shared with this dataset.')
         
+        dataset_owner_ids = [user.user_id for user in dataset.owners]
+        currently_shared_ids = [shared.user_id for shared in SharedDatasetUserModel.query.filter(
+            SharedDatasetUserModel.dataset_id == dataset_id).all()]
+        
         data = request.get_json(force=True)
         
-        shared_user_ids = data.get('users', [])
-        
-        shared_users = UserModel.query.filter(UserModel.user_id.in_(shared_user_ids)).all()
-        currently_shared = [shared.user_id for shared in
-                            SharedDatasetUserModel.query.filter(SharedDatasetUserModel.dataset_id == dataset_id).all()]
-        
-        for user in shared_users:
-            if user in dataset.owners:
+        for user_object in data:
+            if user_object.get('user_id') in dataset_owner_ids:
                 abort(400, 'Dataset cannot be shared with owner.')
-            if user.user_id in currently_shared:
+            if user_object.get('user_id') in currently_shared_ids:
                 abort(400, f'Dataset is already shared with user {user.email}')
+            shared_user_object = SharedDatasetUserModel(user_id=user_object.get('user_id'),
+                                                        dataset_id=dataset.dataset_id)
+            permission_objects = PIIModel.query.filter(
+                (PIIModel.description.in_(user_object.get('permissions', [])))).all()
+            shared_user_object.permissions = permission_objects
             
-            db.session.add(SharedDatasetUserModel(user_id=user.user_id, dataset_id=dataset.dataset_id))
+            db.session.add(shared_user_object)
         
         db.session.commit()
         
         return None, 201
+    
+    @authenticate_token
+    def put(self, user_id, dataset_id):
+        pass
+    
+    @authenticate_token
+    def delete(self, user_id, dataset_id):
+        dataset = DatasetModel.query.filter_by(dataset_id=dataset_id).first()
+        user = UserModel.query.filter_by(user_id=user_id).first()
+        
+        if user not in dataset.owners:
+            abort(401, 'User does not have permission to view users shared with this dataset.')
+        
+        data = request.get_json(force=True)
+        
+        currently_shared_ids = [shared.user_id for shared in SharedDatasetUserModel.query.filter(
+            SharedDatasetUserModel.dataset_id == dataset_id).all()]
+        
+        for user_object in data:
+            deleted_user_id = user_object.get('user_id')
+            if deleted_user_id not in currently_shared_ids:
+                abort(400, f'User {deleted_user_id} does not have access to the dataset.')
+            
+            SharedDatasetUserModel.query.filter(
+                (SharedDatasetUserModel.dataset_id == dataset.dataset_id) & (
+                        SharedDatasetUserModel.user_id == deleted_user_id)).delete()
+        
+        db.session.commit()
+        
+        return None, 204
--- tests/dataset_shared_user_test.py
+++ tests/dataset_shared_user_test.py
@@ -25,7 +25,7 @@ class DatasetSharedUserResourceTest(BaseTest):
     def test_add_dataset_shared_user(self):
         headers = self.generate_auth_headers(user_id=3)
         
-        res = self.client().post(f'{self.dataset_route}/1/user', headers=headers, json={'users': [5]})
+        res = self.client().post(f'{self.dataset_route}/1/user', headers=headers, json=[{'user_id': 5}])
         
         self.assertEqual(res.status_code, 201)
         
@@ -42,7 +42,7 @@ class DatasetSharedUserResourceTest(BaseTest):
     def test_add_dataset_shared_user_already_owner(self):
         headers = self.generate_auth_headers(user_id=3)
         
-        res = self.client().post(f'{self.dataset_route}/2/user', headers=headers, json={'users': [4]})
+        res = self.client().post(f'{self.dataset_route}/2/user', headers=headers, json=[{'user_id': 4}])
         
         self.assertEqual(res.status_code, 400)
         self.assertIn('cannot be shared with owner', res.data.decode())
@@ -50,7 +50,59 @@ class DatasetSharedUserResourceTest(BaseTest):
     def test_add_dataset_shared_user_already_shared(self):
         headers = self.generate_auth_headers(user_id=3)
         
-        res = self.client().post(f'{self.dataset_route}/1/user', headers=headers, json={'users': [1, 2]})
+        res = self.client().post(f'{self.dataset_route}/1/user', headers=headers, json=[
+            {
+                'user_id': 1
+            },
+            {
+                'user_id': 2
+            }
+        ])
         
         self.assertEqual(res.status_code, 400)
         self.assertIn('already shared', res.data.decode())
+    
+    def test_remove_dataset_shared_users(self):
+        headers = self.generate_auth_headers(user_id=3)
+        
+        res = self.client().delete(f'{self.dataset_route}/1/user', headers=headers, json=[
+            {
+                'user_id': 1
+            },
+            {
+                'user_id': 2
+            }
+        ])
+        
+        self.assertEqual(res.status_code, 204)
+        
+        res = self.client().get(f'{self.dataset_route}/1/user', headers=headers)
+        shared_users = json.loads(res.data.decode())
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(shared_users), 0)
+    
+    def test_add_dataset_shared_user_with_permissions(self):
+        headers = self.generate_auth_headers(user_id=3)
+        
+        res = self.client().post(f'{self.dataset_route}/1/user', headers=headers, json=[
+            {
+                'user_id': 5,
+                'permissions': ['ssn', 'name']
+            },
+            {
+                'user_id': 4,
+                'permissions': ['ssn']
+            }
+        ])
+        
+        self.assertEqual(res.status_code, 201)
+        
+        res = self.client().get(f'{self.dataset_route}/1/user', headers=headers)
+        
+        self.assertEqual(res.status_code, 200)
+        shared_users = json.loads(res.data.decode())
+        
+        self.assertEqual(len(shared_users), 4)
+        self.assertEqual(shared_users[2].get('permissions'), [{'description': 'ssn'}, {'description': 'name'}])
+        self.assertEqual(shared_users[3].get('permissions'), [{'description': 'ssn'}])
