3fcc36c67bc2c088ba242355bfc19a39c15103e8
Yash Gunapati <venkat9yash@gmail.com>
2020-06-20 02:43:50 -0400

    Fixing current test cases for user and adding new test cases

--- resources/user.py
+++ resources/user.py
@@ -23,15 +23,14 @@ class UserCollection(Resource):
         """
         args = request.args
         query = f'%{args.get("query", None)}%'
-
         user = UserModel.query.filter_by(user_id=user_id).first()
+        if not user:
+            abort(404, UserErrors.USER_NOT_FOUND)
+            
         user_domain = user.email.split("@")[1]
-
         domains = set(UserConstants.PUBLIC_DOMAINS + [user_domain])
         domain_filters = [f"%{public_domain}%" for public_domain in domains]
 
-        if not user:
-            abort(404, UserErrors.USER_NOT_FOUND)
 
         if args.get("query"):
             user_filter_query = UserModel.query.filter(
