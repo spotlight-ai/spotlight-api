143139c98b8be36a6d358166ae9eba57acfe21e0
Austin Osborne <austin@goaiq.com>
2020-07-14 13:01:40 -0400

    feat: added functionality for storing slack tokens

--- core/errors.py
+++ core/errors.py
@@ -19,7 +19,9 @@ class DatasetErrors:
     MUST_HAVE_OWNER = "Dataset must have at least one owner."
     DOES_NOT_EXIST = "Dataset does not exist."
     NO_NEW_OWNERS = "No new owners to be added."
-    GIVEN_USERS_DO_NOT_OWN = "Cannot process this request as user(s) {not_an_owner} are not the owner(s) of this dataset."    
+    GIVEN_USERS_DO_NOT_OWN = "Cannot process this request as user(s) {not_an_owner} are not the owner(s) of this " \
+                             "dataset."
+
 
 class UserErrors:
     USER_NOT_FOUND = "User or users not found."
@@ -37,3 +39,7 @@ class NotificationErrors:
 
 class DatabaseErrors:
     ISSUE_WRITING_TO_DB = "There was an issue writing to the database. Ensure query params are the correct type."
+
+
+class SlackErrors:
+    TOKEN_ALREADY_EXISTS = 'A token already exists for this team ID.'
