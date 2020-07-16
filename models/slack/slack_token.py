5bf6b4ab93a44de611c049e79527bd47acdecd72
Austin Osborne <austin@goaiq.com>
2020-07-16 13:22:56 -0400

    feat: added user table for slack tokens

--- models/slack/slack_token.py
+++ models/slack/slack_token.py
@@ -9,10 +9,12 @@ class SlackTokenModel(db.Model):
     bot_token = db.Column(db.String, nullable=False)
     bot_id = db.Column(db.String, nullable=False)
     bot_user_id = db.Column(db.String, nullable=False)
+    user_token = db.Column(db.String, nullable=True)
     
     def __repr__(self):
         return f"SlackTokensModel(team_id={self.team_id}, enterprise_id={self.enterprise_id}, " \
-               f"bot_token={self.bot_token}, bot_id={self.bot_id}, bot_user_id={self.bot_user_id}"
+               f"bot_token={self.bot_token}, bot_id={self.bot_id}, bot_user_id={self.bot_user_id}, " \
+               f"user_token={self.user_token}"
     
     def __str__(self):
         return f"SlackBotToken - Team: {self.team_id}, Enterprise: {self.enterprise_id}"
