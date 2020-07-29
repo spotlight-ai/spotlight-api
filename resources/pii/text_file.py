32d02862fb9870a296ee09332d307520c78012ac
krnitish9 <38288221+krnitish9@users.noreply.github.com>
2020-07-29 22:14:10 +0530

    feat: deletion of pii markers from database (#96)
    * fix redaction
    
    * bug: Job check should be done only when user is requesting
    
    * feat: delete pii markers by marker_id (#95)
    
    * small change
    
    * renamed the classes
    
    Co-authored-by: krnitish9 <krnitish9@github.com>

--- resources/pii/text_file.py
+++ resources/pii/text_file.py
@@ -4,12 +4,13 @@ from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
 
 from core.decorators import authenticate_token
+from core.errors import DatasetErrors
 from db import db
 from models.pii.text_file import TextFilePIIModel
+from models.datasets.flat_file import FlatFileDatasetModel
 from schemas.pii.text_file import TextFilePIISchema
 
 text_file_pii_schema = TextFilePIISchema()
-
 
 class TextFilePIICollection(Resource):
     @authenticate_token
@@ -28,8 +29,30 @@ class TextFilePIICollection(Resource):
         return
 
 
-class TextFilePII(Resource):
+class TextFilePIIDatasetCollection(Resource):
     @authenticate_token
     def get(self, user_id, dataset_id):
         pii = TextFilePIIModel.query.filter_by(dataset_id=dataset_id).all()
         return text_file_pii_schema.dump(pii, many=True)
+
+class TextFilePII(Resource):        
+    @authenticate_token
+    def delete(self, user_id, marker_id):
+        try:
+            pii = TextFilePIIModel.query.filter_by(pii_id=marker_id).first()
+            dataset = FlatFileDatasetModel.query.filter_by(dataset_id=pii.dataset_id).first()
+            owners = [owner.user_id for owner in dataset.owners]
+            if user_id not in owners:
+                abort(400, DatasetErrors.USER_DOES_NOT_OWN)
+            else:
+                TextFilePIIModel.query.filter_by(pii_id=marker_id).delete()
+                db.session.commit()
+                return None, 200
+        except ValidationError as err:
+            abort(422, err.messages)
+        except IntegrityError as err:
+            db.session.rollback()
+            abort(400, err)
+        return
+    
+        
