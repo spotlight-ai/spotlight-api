12af3ebf7520ac19bec452b06e71b1d9993ee767
Austin Osborne <austin@goaiq.com>
2020-08-17 14:48:02 -0400

    feat: adjusted audit schema to include user and dataset details

--- schemas/audit/dataset_action_history.py
+++ schemas/audit/dataset_action_history.py
@@ -1,9 +1,13 @@
 from db import ma
 from models.audit.dataset_action_history import DatasetActionHistoryModel
+from schemas.user import UserSchema
+from schemas.datasets.base import DatasetSchema
 
 
 class DatasetActionHistorySchema(ma.ModelSchema):
     class Meta:
         model = DatasetActionHistoryModel
-        include_fk = True
         dump_only = ("timestamp",)
+
+    user = ma.Nested(UserSchema)
+    dataset = ma.Nested(DatasetSchema)
