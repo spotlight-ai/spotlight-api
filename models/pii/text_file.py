1fcce8c600fab50f857382ea8e05cf596b434ea8
Austin Osborne <austin@goaiq.com>
2020-08-21 15:12:31 -0400

    fix: fixed datatype on model

--- models/pii/text_file.py
+++ models/pii/text_file.py
@@ -8,7 +8,7 @@ class TextFilePIIModel(db.Model):
     __tableargs__ = db.UniqueConstraint(
         "pii_id", "dataset_id", "start_location", "end_location"
     )
-
+    
     pii_id = db.Column(db.Integer, primary_key=True)
     dataset_id = db.Column(
         db.Integer, db.ForeignKey("dataset.dataset_id", ondelete="cascade")
@@ -17,17 +17,17 @@ class TextFilePIIModel(db.Model):
     start_location = db.Column(db.Integer, nullable=False)
     end_location = db.Column(db.Integer, nullable=False)
     confidence = db.Column(db.Float, nullable=False)
-    page_number = db.Column(db.Integer, nullable=False, server_default=1)
+    page_number = db.Column(db.Integer, nullable=False, server_default="1")
     last_updated_ts = db.Column(db.DateTime, nullable=False)
-
+    
     def __init__(self, dataset_id, pii_type, start_location, end_location, confidence, page_number=1):
         self.dataset_id = dataset_id
         self.pii_type = pii_type
         self.start_location = start_location
         self.end_location = end_location
         self.confidence = confidence
-        self.page_number = page_number  
+        self.page_number = page_number
         self.last_updated_ts = datetime.utcnow()
-
+    
     def __repr__(self):
         return f"<TextFilePII {self.pii_type}: {self.start_location} - {self.end_location} ({self.confidence}%)"
