632b2538d6433e761bbdb8a4e1d9c7897bd89fd3
krnitish9 <38288221+krnitish9@users.noreply.github.com>
2020-08-25 00:08:40 +0530

    feat: audit event when a dataset has been shared with a user (#104)
    Co-authored-by: krnitish9 <krnitish9@github.com>

--- core/constants.py
+++ core/constants.py
@@ -2,7 +2,7 @@ class AuditConstants:
     DATASET_CREATED = "DATASET_CREATED"
     DATASET_VERIFIED = "DATASET_VERIFIED"
     DATASET_OWNERS_MODIFIED = "Dataset owners list modified"
-
+    DATASET_SHARED = "DATASET_SHARED"    
 
 class NotificationConstants:
     DATASET_SHARED_TITLE = "A dataset has been shared with you"
