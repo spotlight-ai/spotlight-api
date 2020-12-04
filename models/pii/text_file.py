0ffaecb4e321afe1d7e87d9c7bf7d561bd5e44bb
AtulCIS <atul.s@cisinlabs.com>
2020-12-04 18:28:50 +0530

    Removed page_number column from pii_text_file

--- models/pii/text_file.py
+++ models/pii/text_file.py
@@ -17,16 +17,14 @@ class TextFilePIIModel(db.Model):
     start_location = db.Column(db.Integer, nullable=False)
     end_location = db.Column(db.Integer, nullable=False)
     confidence = db.Column(db.Float, nullable=False)
-    page_number = db.Column(db.Integer, nullable=False, server_default="1")
     last_updated_ts = db.Column(db.DateTime, nullable=False)
     
-    def __init__(self, dataset_id, pii_type, start_location, end_location, confidence, page_number=1):
+    def __init__(self, dataset_id, pii_type, start_location, end_location, confidence):
         self.dataset_id = dataset_id
         self.pii_type = pii_type
         self.start_location = start_location
         self.end_location = end_location
         self.confidence = confidence
-        self.page_number = page_number
         self.last_updated_ts = datetime.utcnow()
     
     def __repr__(self):
