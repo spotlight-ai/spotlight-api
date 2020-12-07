c18192d2473c734e1db4d183476753b2e7d390df
AtulCIS <atul.s@cisinlabs.com>
2020-12-07 15:16:01 +0530

    Added functionality to store file_id in Textfile API

--- models/pii/text_file.py
+++ models/pii/text_file.py
@@ -13,14 +13,18 @@ class TextFilePIIModel(db.Model):
     dataset_id = db.Column(
         db.Integer, db.ForeignKey("dataset.dataset_id", ondelete="cascade")
     )
+    file_id = db.Column(
+        db.Integer, db.ForeignKey("flat_file_dataset.id")
+    )
     pii_type = db.Column(db.String, nullable=False)
     start_location = db.Column(db.Integer, nullable=False)
     end_location = db.Column(db.Integer, nullable=False)
     confidence = db.Column(db.Float, nullable=False)
     last_updated_ts = db.Column(db.DateTime, nullable=False)
     
-    def __init__(self, dataset_id, pii_type, start_location, end_location, confidence):
+    def __init__(self, dataset_id,file_id, pii_type, start_location, end_location, confidence):
         self.dataset_id = dataset_id
+        self.file_id = file_id
         self.pii_type = pii_type
         self.start_location = start_location
         self.end_location = end_location
