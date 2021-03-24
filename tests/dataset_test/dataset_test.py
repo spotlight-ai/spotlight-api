86c7a7ae306db4e02a4be56e0d145659697beddd
Joy Kim <73724991+jmorae@users.noreply.github.com>
2021-03-24 10:33:49 -0400

    fix: existing dataset tests

--- tests/dataset_test/dataset_test.py
+++ tests/dataset_test/dataset_test.py
@@ -11,23 +11,6 @@ import pytest
 def mock_api_call(mocker):
     return mocker.patch("botocore.client.BaseClient._make_api_call")
 
-def test_update_owners(client, db_session, mock_api_call):
-    """Verifies that a dataset owner is able to update the owner list."""
-    headers = generate_auth_headers(client, user_id=3)
-
-    res = client.put(
-        f"{dataset_route}/1", headers=headers, json={"owners": [3, 4]}
-    )
-    assert res.status_code == 200
-
-    headers = generate_auth_headers(client, user_id=4)
-    res = client.get(f"{dataset_route}/1", headers=headers)
-    assert res.status_code == 200
-    owners = json.loads(res.data.decode()).get("owners")
-
-    assert len(owners) == 2
-
-
 def test_delete_dataset(client, db_session, mock_api_call):
     """Verifies that datasets can be deleted."""
     headers = generate_auth_headers(client, user_id=3)
